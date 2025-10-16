2c73e32e21a2423dc4a8f8eb5d7ebe1960480d1b
Zachariah M Ellis <zachariah.m.ellis@gmail.com>
2025-10-16 16:00:44 -0700

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// this is the format for a single-line comment
+
+/* This is format for a multi-line comment.
+It can span multiple lines. */
+
+let myName='Max';
+document.write(myName);
+
+let myAge=45;
+document.write(myAge);
+
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+let size = 10;
+size = size * 7;
+document.write(size*7);
