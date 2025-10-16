3a3ae90469462427b8d3850307ae7d52feda3a60
Zachariah M Ellis <zachariah.m.ellis@gmail.com>
2025-10-16 16:51:24 -0700

    Exercise1.1 Changes

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// this is the format for a single-line comment
+
+/* This is format for a multi-line comment.
+It can span multiple lines. */
+
+alert("Hello world"); // This line shows an alert box with the message "Hello world"
+
+let favoriteFood = "Sashimi"; // This line declares a variable named myFavFood and assigns it the value "Sashimi"
+document.write(favoriteFood); // This line writes the value of the variable myFavFood to the HTML document
