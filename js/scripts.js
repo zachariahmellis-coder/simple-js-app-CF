4b9db892a790cffd3e8735159354752f7ed27058
Zachariah M Ellis <zachariah.m.ellis@gmail.com>
2025-10-16 20:27:59 -0700

    Revised heights for object list in .js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 
 //array of objects
-let pokemonlist=[{name: 'Bouffalant', height: '5\'\ 03"', types: ['Normal,Thinking']},
-                    {name: 'Yveltal', height: '19\'\ 00"', types: ['Dark, Flying']},
-                    {name: 'Haunter', height: '5\'\ 03"', types: ['Ghost, Poison']},
-                    {name: 'Noctowl', height: '5\'\ 03"', types: ['Normal, Flying']},
-                    {name: 'Solosis', height: '1\'\ 00"', types: ['Psychic, Coding']},
-                    {name: 'Melmetal', height: '8\'\ 02"', types: ['Steel, Sitting Around All Day']},
+let pokemonlist=[{name: 'Bouffalant', height: 5, types: ['Normal,Thinking']},
+                    {name: 'Yveltal', height: 19, types: ['Dark, Flying']},
+                    {name: 'Haunter', height: 5, types: ['Ghost, Poison']},
+                    {name: 'Noctowl', height: 5, types: ['Normal, Flying']},
+                    {name: 'Solosis', height: 1, types: ['Psychic, Coding']},
+                    {name: 'Melmetal', height: 8, types: ['Steel, Sitting Around All Day']},
 ];
