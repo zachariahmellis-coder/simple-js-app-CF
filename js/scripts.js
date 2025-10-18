38ef194a9ac15b9320b53805ceb973e9cb6f8f96
Zachariah M Ellis <zachariah.m.ellis@gmail.com>
2025-10-18 12:16:08 -0700

    For loop addition to styles.js
    Created a for loop and used doucment.write to displays for DOM

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,26 @@
-
-//array of objects
-let pokemonlist=[{name: 'Bouffalant', height: 5, types: ['Normal,Thinking']},
-                    {name: 'Yveltal', height: 19, types: ['Dark, Flying']},
-                    {name: 'Haunter', height: 5, types: ['Ghost, Poison']},
-                    {name: 'Noctowl', height: 5, types: ['Normal, Flying']},
-                    {name: 'Solosis', height: 1, types: ['Psychic, Coding']},
-                    {name: 'Melmetal', height: 8, types: ['Steel, Sitting Around All Day']},
+//0ption+shift+f to format
+let pokemonList = [
+  { name: 'Bouffalant', height: 5, types: ['Normal, Thinking'] },
+  { name: 'Yveltal', height: 19, types: ['Dark, Flying'] },
+  { name: 'Haunter', height: 5, types: ['Ghost, Poison'] },
+  { name: 'Noctowl', height: 5, types: ['Normal, Flying'] },
+  { name: 'Solosis', height: 1, types: ['Psychic, Coding'] },
+  { name: 'Melmetal', height: 8, types: ['Steel, Sitting Around All Day'] },
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  /*for loop that iterates over each item in pokemonList*/
+  /*Part 1 - Below the pokemonList array in your “scripts.js” file, create a for loop that iterates over each item in pokemonList*/
+  let pokemon = pokemonList[i];
+  let output = `${pokemon.name} (height: ${
+    pokemon.height
+  }) types: ${pokemon.types.join(', ')}`;
+  if (pokemon.height > 8) {
+    output +=
+      ' - Wow, that’s big!'; /*Next, add code to highlight special Pokémon in your list.*/
+  } /*Use what you’ve learned about adding strings in JavaScript to write the Pokémon’s height next to its name, for example, “Bulbasaur (height: 7)”.*/
+  document.write(
+    output + '<br>',
+    '<br>'
+  ); /*Use document.write() inside the loop’s code to write the Pokémon name on your website’s DOM.*/
+}
