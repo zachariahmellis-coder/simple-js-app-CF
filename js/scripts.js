455f6d05121ac00cd81412834bfcca54b729c8c7
Zachariah M Ellis <zachariah.m.ellis@gmail.com>
2025-10-20 15:19:56 -0700

    Corrected ForEach loop code

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-//option+shift+f to format
+///option+shift+f to format
 let pokemonRepository = (function () {
   let pokemonList = [
     { name: 'Bouffalant', height: 5, types: ['Normal, Thinking'] },
     { name: 'Yveltal', height: 19, types: ['Dark, Flying'] },
     { name: 'Haunter', height: 5, types: ['Ghost, Poison'] },
     { name: 'Noctowl', height: 5, types: ['Normal, Flying'] },
-    { name: 'Solosis', height: 1, types: ['Psychic, Coding'] },
+    { name: 'Solosis', height: 2, types: ['Psychic, Coding'] },
     { name: 'Melmetal', height: 8, types: ['Steel, Sitting Around All Day'] },
   ];
 
@@ -19,8 +19,13 @@ let pokemonRepository = (function () {
   };
 })(); //this is an IIFE
 
-console.log(
-  pokemonRepository.getAll()
-); /*loop code updated to cope with new changes*/
-pokemonRepository.add({ name: 'Zacian', height: 9, types: ['Fairy'] });
-console.log(pokemonRepository.getAll());
+//updated forEach loop to retrieve array from IIFE
+pokemonRepository.getAll().forEach(function (pokemon) {
+  let output = `${pokemon.name} (height: ${
+    pokemon.height + ' feet) possess the following specialty(ies):'
+  } ${pokemon.types.join(', ') + '.'}`;
+  if (pokemon.height > 8) {
+    output += ' - Wow, that’s big!';
+  } //used placeholder text for types ${}
+  document.write(output + '<br>', '<br>');
+});
